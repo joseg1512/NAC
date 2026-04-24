a3fabd6a7c565cab77b1d7bd9d2bff0d7886296c
Jose Gabriel Granados <josegabriel.granados@gmail.com>
2026-04-24 17:01:14 -0600

    keepalived changes and notify scripts examples

---
+++ notify02.sh
@@ -0,0 +1,18 @@
+TYPE=$1
+NAME=$2
+STATE=$3
+case $STATE in
+"MASTER") route add default gw 192.168.154.1 dev ens192
+exit 0
+;;
+"BACKUP") route del default gw 192.168.154.1 dev ens192
+exit 0
+;;
+"FAULT") route add default gw 192.168.154.1 dev ens192
+service haproxy restart
+exit 0
+;;
+*) echo "unknown state"
+exit 1
+;;
+esac
